b0b7631bd2acf3f65a9115ef405c628d18cd7331
Darius Valancauskas <darius.valanciauskas@agmis.lt>
2022-05-13 10:30:01 +0300

    Migration

---
+++ migrations/Version20220513062953.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220513062953 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE path (id INT AUTO_INCREMENT NOT NULL, external_id VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE review (id INT AUTO_INCREMENT NOT NULL, path_id INT DEFAULT NULL, vote INT NOT NULL, comment VARCHAR(255) NOT NULL, INDEX IDX_794381C6D96C566B (path_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE review ADD CONSTRAINT FK_794381C6D96C566B FOREIGN KEY (path_id) REFERENCES path (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE review DROP FOREIGN KEY FK_794381C6D96C566B');
+        $this->addSql('DROP TABLE path');
+        $this->addSql('DROP TABLE review');
+    }
+}
